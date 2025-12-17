ef831eae22fd9f3816d425ddf86f0e38c009e908
Mishal Al Shahari <mishalalshahari@gmail.com>
2025-12-17 08:08:12 +0530

    setup

---
+++ src/main/java/com/project/letmenotify/LetMeNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.project.letmenotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetMeNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetMeNotifyApplication.class, args);
+    }
+
+}
